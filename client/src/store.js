f068a98a89fc37f8fe770178f4bc8901cf092649
Jadenpoole3 <jadenkinpool@yahoo.com>
2018-01-04 15:18:30 -0500

    i pretty much finished the logic for the game the immunablity was the hardest part there is a lot of things to think about espcially with the copying the board and update it when a player clicks on it. I cant test it yet i need to style it so I can test it

--- client/src/store.js
+++ client/src/store.js
@@ -19,7 +19,15 @@ function reducer(state, action) {
     //any time a player drops a tile Im going to make it so It will go from true to false using the boolean method 
     if(action.type === 'DROP_TILE') {
         console.log('dropping onto col ' + action.payload);
-        const title = state.current; //depending the on who turn it is 
+        const title = state.current; //depending the on who turn it is the color title its going in 
+        const col = state.board[action.payload].concat(title); //look this part up our new col as well 
+        const board = state.board.slice(); // copying the board directing and cant change the board directly
+        board[action.payload] = col;  //we dont want a copy just a copy with the update with the new tile
+
+        return {
+            current: state.current === 'red' ? 'black' : 'red',  //if the current player color is red then make him turn black and if he is red then make him black
+            board: board,
+        };
     }
     return state;
 }
